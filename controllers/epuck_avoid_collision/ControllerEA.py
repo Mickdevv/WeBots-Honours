d5acdfa9c80c8ec5e4658408dd50f41b6a29b5c4
Mickdevv <46746774+Mickdevv@users.noreply.github.com>
2020-11-09 09:26:48 +0000

    small changes

--- controllers/epuck_avoid_collision/ControllerEA.py
+++ controllers/epuck_avoid_collision/ControllerEA.py
@@ -3,12 +3,11 @@ print("Hello")
 import csv
 
 def main():
-    f = open("EAToController.txt", "x")
+    f = open("EAToController.txt", "w")
     NNValues = [1, 2]
     for i in range(len(NNValues)):
         f.write(str(NNValues[i]) + ", ")
-    print(f.read())
-    f.close()
+    
 
 main()
 
